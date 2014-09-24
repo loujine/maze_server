ff740541b878d51f6332927352a90d9abdc3390c
Stephen Mizell <smizell@gmail.com>
2014-09-23 21:59:10 -0500

    Update to newest Flask wrapper

--- app.py
+++ app.py
@@ -1,68 +1,26 @@
 import os
 from flask import Flask, Response, request, abort
-from hypermedia_resource import HypermediaResource
-from hypermedia_resource.contrib.browser import BrowserAdapter
-from hypermedia_resource.contrib.maze_xml import MazeXMLAdapter
-from hypermedia_resource.wrappers import HypermediaResponse, ResponseBuilder
-import maze
+from resources import CellResource, ExitResource, RootResource
 
 app = Flask(__name__)
-HypermediaResource.adapters.add(BrowserAdapter)
-HypermediaResource.adapters.add(MazeXMLAdapter)
-
-# Helper functions for the views
-
-def maze_resource(type_of):
-    """
-    Sets up a HypermediaResource for the resource
-    """
-    resource = HypermediaResource()
-    resource.meta.attributes.add("title", "Hypermedia Maze")
-    resource.meta.attributes.add("type", type_of)
-    return resource
-
-def maze_response(resource):
-    """
-    Build a HypermediaResponse
-    """
-    response_builder = ResponseBuilder("application/vnd.amundsen.maze+xml")
-    response = response_builder.build(resource, request.headers.get("Accept"))
-    return Response(response.body, mimetype=response.media_type)
 
 # Route and views
 
 @app.route('/', methods=["GET"])
 def root():
-    """
-    Root resource
-    """
-    resource = maze_resource(type_of='item')
-    resource.links.add(rel='start', href=maze.link_to_cell(0), label="Start")
-    return maze_response(resource)
+    return RootResource().response_for(request)
 
 @app.route('/cells/999', methods=["GET"])
 def exit():
-    """
-    Exit resource
-    """
-    resource = maze_resource(type_of='completed')
-    resource.links.add(rel='start', href=maze.link_to_cell(0), label="Start")
-    return maze_response(resource)
+    return ExitResource().response_for(request)
 
-@app.route('/cells/<cell_num>', methods=["GET"])
+@app.route('/cells/<int:cell_num>', methods=["GET"])
 def cell(cell_num):
-    """
-    Cell resource
-    """
-    resource = maze_resource(type_of='cell')
-    cell = int(cell_num)
-    if not maze.has_cell(cell):
-        abort(404)
-    links = maze.get_links_for_cell(cell)
-    for rel, link in links.iteritems():
-        resource.links.add(rel=rel, href=link, label=maze.labels[rel])
-    return maze_response(resource)
+    resource = CellResource()
+    resource.cell_num = cell_num
+    return resource.response_for(request)
 
 if __name__ == "__main__":
+    app.debug = True
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
---
+++ resources.py
@@ -0,0 +1,44 @@
+from hypermedia_resource import HypermediaResource
+from hypermedia_resource.contrib.browser import BrowserAdapter
+from hypermedia_resource.contrib.maze_xml import MazeXMLAdapter
+from hypermedia_resource.wrappers import FlaskAPIResource
+import maze
+
+HypermediaResource.adapters.add(BrowserAdapter)
+HypermediaResource.adapters.add(MazeXMLAdapter)
+
+class MazeResource(FlaskAPIResource):
+
+    def maze_resource(self, type_of):
+        """
+        Sets up a HypermediaResource for the resource
+        """
+        resource = HypermediaResource()
+        resource.meta.attributes.add("title", "Hypermedia Maze")
+        resource.meta.attributes.add("type", type_of)
+        return resource
+
+class RootResource(MazeResource):
+
+    def read(self, request):
+        resource = self.maze_resource(type_of='item')
+        resource.links.add(rel='start', href=maze.link_to_cell(0), label="Start")
+        return resource
+
+class ExitResource(MazeResource):
+
+    def read(self, request):
+        resource = self.maze_resource(type_of='completed')
+        resource.links.add(rel='start', href=maze.link_to_cell(0), label="Start")
+        return resource
+
+class CellResource(MazeResource):
+
+    def read(self, request):
+        resource = self.maze_resource(type_of='cell')
+        if not maze.has_cell(self.cell_num):
+            abort(404)
+        links = maze.get_links_for_cell(self.cell_num)
+        for rel, link in links.iteritems():
+            resource.links.add(rel=rel, href=link, label=maze.labels[rel])
+        return resource
